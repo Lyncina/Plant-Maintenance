03a845291e40630efa4c97ebbcdf1171eed19699
Lyn <KeitorinM@gmail.com>
2025-07-07 09:22:30 -0400

    Adding more information.

--- js/makeItWork.js
+++ js/makeItWork.js
@@ -1,4 +1,17 @@
+const infoD = document.getElementById("D-info");
+const infoT = document.getElementById("T-info");
 const infoGH = document.getElementById("GH-info");
 const infoFH = document.getElementById("FH-info");
 const infoSS = document.getElementById("SS-info");
+const infoBI = document.getElementById("BI-info");
 const infoBH = document.getElementById("BH-info");
+const infoP = document.getElementById("P-info");
+const infoC = document.getElementById("C-info");
+const infoSDOD = document.getElementById("SDOD-info");
+const infoMTD = document.getElementById("MTD-info");
+const infoOD = document.getElementById("OD-info");
+const infoOMH = document.getElementById("OMH-info");
+
+function showAll () {
+  
+}
